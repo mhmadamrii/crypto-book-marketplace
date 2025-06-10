f5c62c04c2e60a9306b49b2018f9b886e6c11933
mhmadamrii <interceptorghost4@gmail.com>
2025-06-10 09:57:30 +0700

    fix: added metadata uri ipfs

--- apps/smart-contract/contracts/BookMarketplace.sol
+++ apps/smart-contract/contracts/BookMarketplace.sol
@@ -8,6 +8,8 @@ contract BookMarketplace is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _bookIds;
 
+    address public owner;
+
     mapping(uint256 => Book) public books;
     mapping(address => uint256[]) public ownedBooks;
 
@@ -26,9 +28,14 @@ contract BookMarketplace is ReentrancyGuard {
         uint256 id;
         address payable author;
         string ipfsMetadataHash;
+        string metadataURI; // IPFS URI
         uint256 price;
         uint256 royaltyPercent;
         bool exists;
+    }
+
+    constructor() {
+        owner = msg.sender;
     }
 
     function createBook(
@@ -40,5 +47,38 @@ contract BookMarketplace is ReentrancyGuard {
         require(_royaltyPercent <= 100, "Royalty too high");
 
         uint256 bookId = _bookIdCounter.current();
+        books[bookId] = Book({
+            id: bookId,
+            author: payable(msg.sender),
+            metadataURI: _metadataURI,
+            price: _price,
+            royaltyPercent: _royaltyPercent,
+            exists: true
+        });
+
+        _bookIdCounter.increment();
+        emit BookCreated(bookId, msg.sender, _price);
+    }
+
+    function buyBook(uint256 _bookId) external payable nonReentrant {
+        Book memory book = books[_bookId];
+        require(book.exists, "Book does not exist");
+        require(msg.value == book.price, "Incorrect pyament amount");
+
+        book.author.transfer(book.price);
+        emit BookPurchased(_bookId, msg.sender, book.price);
+    }
+
+    function getAllBooks() external view returns (Book[] memory) {
+        Book[] memory allBooks = new Book[](_bookIdCounter.current());
+        uint256 i = 0;
+        for (uint256 bookId = 1; bookId <= _bookIdCounter.current(); bookId++) {
+            Book memory book = books[bookId];
+            if (book.exists) {
+                allBooks[i] = book;
+                i++;
+            }
+        }
+        return allBooks;
     }
 }
